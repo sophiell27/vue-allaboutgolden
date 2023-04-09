cf04813829e70670d65ecc707be81d8e47b4e073
sophiell27 <105938870+sophiell27@users.noreply.github.com>
2023-04-09 21:21:18 +0800

    fix: adminLogin loader

--- src/stores/adminStore.js
+++ src/stores/adminStore.js
@@ -1,120 +1,109 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia';
 import axios from 'axios';
-import router from "../router";
-import {useLoading, LoadingPlugin} from 'vue-loading-overlay';
-import Myloader from "../components/MyLoader.vue"
-import Swal from "sweetalert2";
-// console.log(Swal);
-const  { VITE_API, VITE_PATH } = import.meta.env;
-export default defineStore(
-    "adminStore", {
-        state: ()=> ({
-            products: [],
-            productsPagination: {},
-            orders: [],
-            orderPagination: {}
-        }),
-        
-        actions: {
-            playLoading(){
-                const  loading = useLoading ();
-                const loader =loading.show({
-                    loader: Myloader,
-                });
-                setTimeout(()=> {
-                    loader.hide()
-                },1000)
-            },
+import router from '../router';
+import Swal from 'sweetalert2';
+const { VITE_API, VITE_PATH } = import.meta.env;
+export default defineStore('adminStore', {
+  state: () => ({
+    products: [],
+    productsPagination: {},
+    orders: [],
+    orderPagination: {},
+    isLoading: false,
+    fullPage: true,
+  }),
 
-            async checkLogin(path = "/admin"){
-                const token = document.cookie.replace(/(?:(?:^|.*;\s*)goldenToken\s*\=\s*([^;]*).*$)|^.*$/, "$1")
-                axios.defaults.headers.common['Authorization'] = token;
-                
-              const  result= async ()=> {
-                try {
-                        const res = await axios.post(`${VITE_API}api/user/check`);
-                        this.router.push(path);
-                        return true
-                    } catch (res_1) {
-                        this.router.push("/admin/login");
-                        return false
-                    }
-              }
-              return await result();
-              
-                // await axios.post(`${VITE_API}api/user/check`)
-                // .then(res => {
-                //     result= true
-                // })
-                // .catch(err => {
-                //     result  = false
-                // })
-                // console.log(result);
-                // return result 
-                // return rr 
-                // try {
-                //     const res = await axios.post(`${VITE_API}api/user/check`);
-                //     this.router.push("/admin");
-                // } catch (res_1) {
-                //     this.router.push("/admin/login");
-                // }
-            },
-            
-            async getProducts(page = 1) {
-                this.playLoading();
-                await axios.get(`${VITE_API}api/${VITE_PATH}/admin/products?page=${page}`)
-                    .then(res => {
-                        this.products = res.data.products
-                        this.productsPagination = res.data.pagination
-                        this.router.push(`/admin/products/${res.data.pagination.current_page}`)
-               
-                    })
-                    .catch(err => {
-                        // console.dir(err)
-                        alert("取得產品列表發生錯誤")
-                    })
-            },
-            getOrders(page=1) {
-               this.playLoading();
-                axios.get(`${VITE_API}api/${VITE_PATH}/admin/orders?page=${page}`)
-                .then(res => {
-                    this.orders = res.data.orders
-                    this.orderPagination = res.data.pagination;
-                    this.router.push(`/admin/orders/${res.data.pagination.current_page}`)
-                    // this.loader.hide();
-                  
-                })
-                .catch(err => {
-                    console.log("無法取得訂單列表");
-                    console.log(err);
-                    // this.loader.hide();
-                })
-                
-    
-            },
-            async confirmMessage(msg){
-                 return   await  Swal.fire({
-                        text: msg,
-                        confirmButtonText: "確定",
-                        
-                    });
-            },
-            alertMessage(msg) {
-                Swal.fire({
-                  text: msg,
-                  timer: 1000,
-                });
-              },
-            toastMessge(msg, position = "top-end") {
-                Swal.fire({
-                  toast: true,
-                  position ,
-                  icon: "success",
-                  title: msg,
-                  showConfirmButton: false,
-                  timer: 1000,
-                });
-            }
+  actions: {
+    changeLoading(status){
+      this.isLoading = status;
+    },
+    async checkLogin(path = '/admin') {
+      const token = document.cookie.replace(
+        /(?:(?:^|.*;\s*)goldenToken\s*\=\s*([^;]*).*$)|^.*$/,
+        '$1'
+      );
+      axios.defaults.headers.common['Authorization'] = token;
+
+      const result = async () => {
+        try {
+          const res = await axios.post(`${VITE_API}api/user/check`);
+          this.router.push(path);
+          return true;
+        } catch (res_1) {
+          this.router.push('/admin/login');
+          return false;
         }
-    }
-)
+      };
+      return await result();
+
+      // await axios.post(`${VITE_API}api/user/check`)
+      // .then(res => {
+      //     result= true
+      // })
+      // .catch(err => {
+      //     result  = false
+      // })
+      // console.log(result);
+      // return result
+      // return rr
+      // try {
+      //     const res = await axios.post(`${VITE_API}api/user/check`);
+      //     this.router.push("/admin");
+      // } catch (res_1) {
+      //     this.router.push("/admin/login");
+      // }
+    },
+    async getProducts(page = 1) {
+      await axios
+        .get(`${VITE_API}api/${VITE_PATH}/admin/products?page=${page}`)
+        .then((res) => {
+          this.products = res.data.products;
+          this.productsPagination = res.data.pagination;
+          this.router.push(
+            `/admin/products/${res.data.pagination.current_page}`
+          );
+        })
+        .catch((err) => {
+          // console.dir(err)
+          alert('取得產品列表發生錯誤');
+        });
+    },
+    getOrders(page = 1) {
+      axios
+        .get(`${VITE_API}api/${VITE_PATH}/admin/orders?page=${page}`)
+        .then((res) => {
+          this.orders = res.data.orders;
+          this.orderPagination = res.data.pagination;
+          this.router.push(`/admin/orders/${res.data.pagination.current_page}`);
+          // this.loader.hide();
+        })
+        .catch((err) => {
+          console.log('無法取得訂單列表');
+          console.log(err);
+          // this.loader.hide();
+        });
+    },
+    async confirmMessage(msg) {
+      return await Swal.fire({
+        text: msg,
+        confirmButtonText: '確定',
+      });
+    },
+    alertMessage(msg) {
+      Swal.fire({
+        text: msg,
+        timer: 1000,
+      });
+    },
+    toastMessge(msg, position = 'top-end') {
+      Swal.fire({
+        toast: true,
+        position,
+        icon: 'success',
+        title: msg,
+        showConfirmButton: false,
+        timer: 1000,
+      });
+    },
+  },
+});
--- src/views/admin/AdminLogin.vue
+++ src/views/admin/AdminLogin.vue
@@ -1,99 +1,78 @@
 <script>
 import adminStore from '../../stores/adminStore.js';
-import { mapActions } from 'pinia';
+import { mapActions, mapState } from 'pinia';
 const { VITE_API, VITE_PATH } = import.meta.env;
-// import MyloaderComponent from '../../components/MyloaderComponent.vue';
-
 export default {
-    data() {
-        return {
-
-        }
+  data() {
+    return {
+    };
+  },
+  computed: {
+    ...mapState(adminStore, ['isLoading', "fullPage"]),
+  },
+  methods: {
+    ...mapActions(adminStore, ["checkLogin", "alertMessage", "changeLoading"]),
+    login(value) {
+      this.changeLoading(true);
+      const data = {
+        username: value.帳戶名稱,
+        password: value.登入密碼,
+      }
+      this.$http.post(`${VITE_API}/admin/signin`, data)
+        .then(res => {
+          this.changeLoading(false);
+          const { token, expired } = res.data;
+          document.cookie = `goldenToken=${token};expires=${new Date(expired)}`
+          this.$router.push("/admin");
+        })
+        .catch(err => {
+          this.changeLoading(false);
+          this.alertMessage("登入錯誤，請重新輸入");
+        })
     },
-    components: {
-        // MyloaderComponent
-    },
-    methods: {
-        ...mapActions(adminStore, ["checkLogin", "alertMessage"]),
-        login(value) {
-
-            const data = {
-                username: value.帳戶名稱,
-                password: value.登入密碼
-            }
-            const loader = this.$loading.show();
-            this.$http.post(`${VITE_API}/admin/signin`, data)
-                .then(res => {
-                    const { token, expired } = res.data;
-                    document.cookie = `goldenToken=${token};expires=${new Date(expired)}`
-                    this.$router.push("/admin")
-                    loader.hide();
-                    console.log("login", document.cookie);
-                })
-                .catch(err => {
-                    loader.hide();
-                    this.alertMessage("登入錯誤，請重新輸入")
-                    console.log("fail", document.cookie);
-                })
-               
-        }
-
-
-    },
-    mounted() {
-        console.log("mouned", document.cookie);
-        this.checkLogin();
-    }
-}
-
-// {
-//   "username": "example@test.com",
-//   "password": "example"
-// }
-
+  },
+  mounted() {
+    this.checkLogin();
+  },
+};
 </script>
-
 <template>
-    <nav class="bg-primary py-6">
-        <ul class="container flex items-center justify-start text-dark">
-            <li class="">
-                <RouterLink to="/" class="hover:opacity-70 flex items-center">
-                    <span class="material-symbols-outlined">
-                        chevron_left
-                    </span>
-                    回到前台首頁
-                </RouterLink>
-
-            </li>
-        </ul>
-    </nav>
-    <!-- <MyloaderComponent></MyloaderComponent> -->
-    <div class="container flex flex-col items-center justify-center mt-20">
-        <!-- <VLoadingComponent></VLoadingComponent> -->
-        <!-- <MyloaderComponent></MyloaderComponent> -->
-        <v-form class="" @submit="login">
-            <RouterLink to="/">
-                <img src="../../assets/images/Logo.svg" alt="logo" class="mb-8">
-            </RouterLink>
-            <h1 class="text-2xl mb-4 text-center">管理員登入</h1>
-            <div class="mb-4">
-                <label for="adminLoginName" class="block mb-2">帳戶名稱</label>
-                <v-field name="帳戶名稱" type="email" id="adminLoginName"
-                    class="px-4 py-2 border border-primary rounded-lg bg-transparent mb-1 w-full" placeholder="請輸入帳戶名稱"
-                    rules="required|email">
-                </v-field>
-                <v-error-message name="帳戶名稱" class="text-highlight"></v-error-message>
-            </div>
-
-            <div class="mb-4">
-                <label for="adminLoginPw" class="block mb-2">帳戶密碼</label>
-                <v-field name="登入密碼" type="password" id="adminLoginPw"
-                    class="px-4 py-2 border border-primary rounded-lg bg-transparent mb-1 w-full" placeholder="請輸入密碼"
-                    rules="required|min:8">
-                </v-field>
-                <v-error-message name="登入密碼" class="text-highlight"></v-error-message>
-            </div>
-            <button class="bg-secondary rounded-lg text-white w-full px-6 py-2">登入</button>
-        </v-form>
-    </div>
+  <loading v-model:active="isLoading" :is-full-page="fullPage" />
+  <nav class="bg-primary py-6">
+    <ul class="container flex items-center justify-start text-dark">
+      <li class="">
+        <RouterLink to="/" class="hover:opacity-70 flex items-center">
+          <span class="material-symbols-outlined">
+            chevron_left
+          </span>
+          回到前台首頁
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
+  <div class="container flex flex-col items-center justify-center mt-20">
+    <v-form class="" @submit="login">
+      <RouterLink to="/">
+        <img src="../../assets/images/Logo.svg" alt="logo" class="mb-8">
+      </RouterLink>
+      <h1 class="text-2xl mb-4 text-center">管理員登入</h1>
+      <div class="mb-4">
+        <label for="adminLoginName" class="block mb-2">帳戶名稱</label>
+        <v-field name="帳戶名稱" type="email" id="adminLoginName"
+          class="px-4 py-2 border border-primary rounded-lg bg-transparent mb-1 w-full" placeholder="請輸入帳戶名稱"
+          rules="required|email">
+        </v-field>
+        <v-error-message name="帳戶名稱" class="text-highlight"></v-error-message>
+      </div>
+      <div class="mb-4">
+        <label for="adminLoginPw" class="block mb-2">帳戶密碼</label>
+        <v-field name="登入密碼" type="password" id="adminLoginPw"
+          class="px-4 py-2 border border-primary rounded-lg bg-transparent mb-1 w-full" placeholder="請輸入密碼"
+          rules="required|min:8">
+        </v-field>
+        <v-error-message name="登入密碼" class="text-highlight"></v-error-message>
+      </div>
+      <button class="bg-secondary rounded-lg text-white w-full px-6 py-2">登入</button>
+    </v-form>
+  </div>
 </template>
