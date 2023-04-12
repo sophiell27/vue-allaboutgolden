531440c3cb66fdb142243fbe742f2074309aead6
sophiell27 <105938870+sophiell27@users.noreply.github.com>
2023-04-12 21:55:56 +0800

    update: RelatedProductsComponent

--- src/components/RelatedProductsComponent.vue
+++ src/components/RelatedProductsComponent.vue
@@ -6,13 +6,12 @@ export default {
 <template>
   <!-- 上架product  -->
   <ul class="newArrival grid grid-cols-12 md:gap-x-14 md:mb-6">
-    <!-- 87% 91% 98% 100% -->
     <li
-      class="col-span-12 mb-6 text-center overflow-hidden group hover:shadow-lg2 rounded-tl-[7%] rounded-tr-[15%] rounded-bl-[5%] rounded-br-[10%] odd:rounded-br-[7%] odd:rounded-bl-[15%] odd:rounded-tr-[5%] odd:rounded-tl-[10%] md:last:hidden md:col-span-6 lg:col-span-4 lg:last:block"
+      class="col-span-12 mb-6 text-center rounded-lg2 overflow-hidden group hover:shadow-lg2 md:last:hidden md:col-span-6 lg:col-span-4 lg:last:block"
       data-aos="fade-left" v-for="product in products.slice(products.length - 3)" :key="product.id">
       <!-- path: "/products/category/:category/product/:productid", -->
       <RouterLink :to="`/products/category/${product.category}/product/${product.id}`">
-        <img :src="product.imageUrl" alt="" class="mb-2 h-[360px] w-full object-cover rounded-lg2  ">
+        <img :src="product.imageUrl" alt="" class="mb-2 h-[360px] w-full object-cover rounded-t-lg2  ">
         <div class="min-h-[54px] mb-1 text-4.5">
           <h2> {{ product.title }}</h2>
         </div>
