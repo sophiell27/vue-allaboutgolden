77f7590da28010e0ae075d8c9ff9e77d90e2202f
sophiell27 <105938870+sophiell27@users.noreply.github.com>
2023-04-13 00:40:46 +0800

    update: CartComponent

--- src/components/CartsComponent.vue
+++ src/components/CartsComponent.vue
@@ -40,53 +40,81 @@ export default {
 };
 </script>
 <template>
-  <table class="w-full table-fixed text-center mb-6">
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 ">
+      <thead class="font-extrabold text-4.5 text-fog-500 bg-fog-200 whitespace-nowrap">
+        <tr>
+          <th scope="col" class="px-6 py-3 hidden md:block">
+          </th>
+          <th scope="col" class="px-6 py-3">
+            產品
+          </th>
+          <th scope="col" class="px-6 py-3">
+            價錢
+          </th>
+          <th scope="col" class="px-6 py-3">
+            數量
+          </th>
+          <th scope="col" class="px-6 py-3">
+            小計
+          </th>
+          <th scope="col" class="px-6 py-3">
+            <span class="hidden md:block">刪除</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-white border-b hover:bg-gray-50 " v-for="item in carts" :key="item.id">
+          <td class="px-2 py-4 md:px-6 hidden md:block">
+            <img :src="item.product.imageUrl" :alt="item.product.title" class="w-20">
+          </td>
+          <td class="px-2 py-4 md:px-6">
+            {{ item.product.title }}
+          </td>
+          <td class="px-2 py-4 text-xs md:px-6 ">
+            NT $ {{ item.product.price }}
+          </td>
+          <td class="px-2 py-4 md:px-6">
+            <span>
+              <input type="number" min="1" :value="item.qty" class="text-xs border-none w-20 text-center"
+                @change="(evt) => changeQty(item.id, evt)">
+            </span>
+          </td>
+          <td class="px-2 py-4 md:px-6 whitespace-nowrap text-xs">
+            NT $ {{ item.final_total }}
+          </td>
+          <td class="px-2 py-4 md:px-6">
+            <button type="button" class="px-2 hover:text-highlight" @click="delCartItem(item.id)">
+              x
+            </button>
+          </td>
+        </tr>
+        <tr class="bg-white border-b hover:bg-gray-50" v-if="cartTotal < 900">
+          <td class="px-2 py-4 md:px-6 hidden md:block"></td>
+          <td class="px-2 py-4 md:px-6 hidden md:block"></td>
+          <td class="px-2 py-4 md:px-6">運費</td>
+          <td class="px-2 py-4 md:px-6"></td>
+          <td class="px-2 py-4 md:px-6"></td>
+          <td class="px-2 py-4 text-xs md:px-6">NT $ 500</td>
+          <td class="px-2 py-4 md:px-6"></td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr class="font-semibold text-gray-900 dark:text-white">
+          <td class="px-2 py-4 md:px-6 hidden md:block"></td>
+          <td colspan="2"><span v-if="cartTotal >= 900">已達免運費門檻</span> <span v-else>未達免運費門檻</span></td>
+          <!-- <td></td> -->
+          <td class="px-2 py-3 text-base whitespace-nowrap">總計:</td>
+          <td class="px-2 py-3 text-xs whitespace-nowrap md:px-6">NT $ {{ cartTotal >= 900 ? cartTotal : cartTotal + 500
+          }}</td>
+        </tr>
+      </tfoot>
+    </table>
 
-    <thead>
-      <tr class="font-extrabold text-4.5 ">
-        <th></th>
-        <th>產品</th>
-        <th>價錢</th>
-        <th>數量</th>
-        <th>小計</th>
-        <th>刪除</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in carts" class="hover:bg-fog-200" :key="item.id">
-        <td class=" lg:w-24 lg:h-24">
-          <img :src="item.product.imageUrl" :alt="item.product.title" class="w-20">
-        </td>
-        <td>
-          {{ item.product.title }}
-        </td>
-        <td class="text-xs">NT $ {{ item.product.price }}</td>
-        <td class="text-xs">
-          <input type="number" min="1" :value="item.qty" class="w-full text-xs ml-2 text-center  border-none rounded-md bg-transparent md:mx-1 md:py-1 md:w-20 md:pl-2  md:bg-fog-200"
-            @change="(evt) => changeQty(item.id, evt)">
-        </td>
-        <td class="text-xs">NT $ {{ item.final_total }}</td>
-        <td> <button type="button" class="hover:text-highlight" @click="delCartItem(item.id)">
-            x
-          </button></td>
-      </tr>
-    </tbody>
-    <tfoot class="border-t border-t-fog-500 border-spacing-1">
-      <tr class="">
-        <td colspan="3" class="text-end ">
-          <p class="inline-block px-2 bg-fog-200 " v-if="cartTotal >= 900">已達免運費門檻</p>
-        </td>
-        <td class="">總計:</td>
-        <td class="text-xs">NT$ {{ cartTotal }}</td>
-      </tr>
-      <tr>
-        <td colspan="5" v-if="$route.fullPath != '/placeorder'">
-          <RouterLink to="/placeorder">
-            <button type="button"
-              class="py-2 px-12 bg-secondary rounded-lg2 text-white flex ml-auto mt-4 hover:bg-primary hover:text-dark" v-if="$route.fullPath ==='/user/carts'">結帳</button>
-          </RouterLink>
-        </td>
-      </tr>
-    </tfoot>
-  </table>
+  </div>
+  <RouterLink to="/user/placeorder" v-if="$route.fullPath === '/user/carts'">
+    <button type="button"
+      class="py-2 px-12 bg-secondary rounded-lg2 text-white flex ml-auto mt-4 hover:bg-primary hover:text-dark"
+      >結帳</button>
+  </RouterLink>
 </template>
